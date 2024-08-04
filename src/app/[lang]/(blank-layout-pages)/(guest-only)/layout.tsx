0efd9f8d0c88d1f2f01b935aeb12400365c45bfe
Mrinal Singh <mrinalsingh@CRDL-LAP-0045.local>
2024-08-04 12:36:06 +0530

    dependency fixed

--- src/app/[lang]/(blank-layout-pages)/(guest-only)/layout.tsx
+++ src/app/[lang]/(blank-layout-pages)/(guest-only)/layout.tsx
@@ -1,11 +1,12 @@
 // Type Imports
-import type { ChildrenType } from '@core/types'
 import type { Locale } from '@/configs/i18n'
+import type { ChildrenType } from '@core/types'
 
 // HOC Imports
 import GuestOnlyRoute from '@/hocs/GuestOnlyRoute'
 
 const Layout = ({ children, params }: ChildrenType & { params: { lang: Locale } }) => {
+  // @ts-ignore
   return <GuestOnlyRoute lang={params.lang}>{children}</GuestOnlyRoute>
 }
 
