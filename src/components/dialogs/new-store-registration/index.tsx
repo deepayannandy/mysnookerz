29cfdd73b2d9344c7a74aa3d7b784fd8db10100c
Mrinal Singh <mrinalsingh@CRDL-LAP-0045.local>
2025-05-01 23:07:56 +0530

    subscription id in create store

--- src/components/dialogs/new-store-registration/index.tsx
+++ src/components/dialogs/new-store-registration/index.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 // React Imports
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 
 // MUI Imports
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
@@ -47,11 +48,47 @@ const NewStoreInfo = ({ open, setOpen, getStoreData }: NewStoreInfoProps) => {
   //   //subscription: subscriptions[0]
   // })
   const [userData, setUserData] = useState({ validDays: 30 } as NewStoreInfoData)
+  const [subscriptionList, setSubscriptionList] = useState(
+    [] as { _id: string; displayName: string; subscriptionName: string; subscriptionPrice: number }[]
+  )
+  const [selectedPlanId, setSelectedPlanId] = useState('')
 
   const handleClose = () => {
     setOpen(false)
     setUserData({} as NewStoreInfoData)
   }
+
+  const getSubscriptions = async () => {
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL
+    const token = localStorage.getItem('token')
+
+    try {
+      const response = await axios.get(`${apiBaseUrl}/subscription`, { headers: { 'auth-token': token } })
+      if (response && response.data) {
+        const list = [] as { _id: string; displayName: string; subscriptionName: string; subscriptionPrice: number }[]
+
+        response.data.forEach((data: any) => {
+          list.push({
+            ...data,
+            displayName: `${data.subscriptionName} - ₹${data.subscriptionPrice}`
+          })
+        })
+        setSubscriptionList(list)
+        setSelectedPlanId(list[0]?._id)
+      }
+    } catch (error: any) {
+      // if (error?.response?.status === 409) {
+      //   const redirectUrl = `/${locale}/login?redirectTo=${pathname}`
+      //   return router.replace(redirectUrl)
+      // }
+      toast.error(error?.response?.data?.message ?? error?.message, { hideProgressBar: false })
+    }
+  }
+
+  useEffect(() => {
+    getSubscriptions()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -60,7 +97,12 @@ const NewStoreInfo = ({ open, setOpen, getStoreData }: NewStoreInfoProps) => {
     const data = _.omit(userData, 'address', 'city', 'state', 'pincode', 'validDays')
     const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL
     try {
-      const response = await axios.post(`${apiBaseUrl}/store`, { ...data, address, valid_days })
+      const response = await axios.post(`${apiBaseUrl}/store`, {
+        ...data,
+        address,
+        valid_days,
+        subscriptionId: selectedPlanId
+      })
 
       if (response && response.data) {
         getStoreData()
@@ -170,6 +212,31 @@ const NewStoreInfo = ({ open, setOpen, getStoreData }: NewStoreInfoProps) => {
               value={userData?.state}
               onChange={e => setUserData({ ...userData, state: e.target.value })}
             />
+
+            <FormControl fullWidth size='small'>
+              <InputLabel id='user-view-plans-select-label'>Choose Plan</InputLabel>
+              <Select
+                label='Choose Plan'
+                defaultValue='Standard'
+                id='user-view-plans-select'
+                labelId='user-view-plans-select-label'
+                value={selectedPlanId}
+                onChange={event => setSelectedPlanId(event.target.value)}
+              >
+                {subscriptionList.map(
+                  (subscription: {
+                    _id: string
+                    displayName: string
+                    subscriptionName: string
+                    subscriptionPrice: number
+                  }) => (
+                    <MenuItem key={subscription._id} value={subscription._id}>
+                      {subscription.displayName}
+                    </MenuItem>
+                  )
+                )}
+              </Select>
+            </FormControl>
 
             <div className='flex items-center gap-4'>
               <Button variant='contained' type='submit'>
